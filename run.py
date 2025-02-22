0bb6849af7c875cace28d45b759f15df85850d1a
giovannilopez9808 <giovannilopez9808@gmail.com>
2025-02-22 16:36:02 -0600

    Update

--- run.py
+++ run.py
@@ -2,12 +2,12 @@
 from pdf2image import convert_from_path
 from sys import argv
 # Store Pdf with convert_from_path function
-filename = argv[1]
+filename = argv[2]
 filename_image = filename.replace(
     ".pdf", ".jpg"
 )
 document = convert_from_path(
-    argv[1]
+    filename
 )
 document = document[-1]
 document.save(
